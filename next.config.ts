268be25b160490c115c1849658428631fb8c1c1b
Plamen Petrov <biznetmen@gmail.com>
2025-08-03 13:08:11 +0300

    presidents images moved to /public

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.loc.gov',
-        port: '',
-        pathname: '/static/portals/free-to-use/public-domain/presidential-portraits/**',
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
 
