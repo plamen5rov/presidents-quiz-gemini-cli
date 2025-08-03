2ab5cc8dd1a3fe2bd23c841d7dddcba820198bb8
Plamen Petrov <biznetmen@gmail.com>
2025-08-03 12:50:56 +0300

    image load fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.loc.gov',
+        port: '',
+        pathname: '/static/portals/free-to-use/public-domain/presidential-portraits/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
